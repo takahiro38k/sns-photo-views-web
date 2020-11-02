9d6c7e1e0465c9180a685e6960a48297293ca3f6
takahiro38k <grandeur38k@gmail.com>
2020-11-02 12:46:55 +0900

    2020-1102-1246-04 deleted default files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import styles from './App.module.css';
-import Core from './features/core/Core';
+import styles from "./App.module.css";
+import Core from "./features/core/Core";
 
 function App() {
   return (
---
+++ src/features/types.ts
@@ -0,0 +1,69 @@
+/**
+ * file objectの型定義
+ * Blob
+ * Binary Large Object。バイナリデータであるため、テキストファイルだけではなく
+ * 画像やPDFなどいろいろな形式のファイルを扱うことができる。
+ */
+export interface File extends Blob {
+  readonly lastModified: number;
+  readonly name: string;
+}
+
+/**
+ * authSlice.ts
+ */
+export interface PROPS_AUTHEN {
+  email: string;
+  password: string;
+}
+
+export interface PROPS_PROFILE {
+  id: number;
+  nickName: string;
+  img: File | null;
+}
+
+export interface PROPS_NICKNAME {
+  nickName: string;
+}
+
+/**
+ * postSlice.ts
+ */
+export interface PROPS_NEWPOST {
+  title: string;
+  img: File | null;
+}
+
+export interface PROPS_LIKED {
+  // 投稿のid
+  id: number;
+  // 投稿のtitle
+  title: string;
+  // 既存のいいねuserのid
+  current: number[];
+  // 新規のいいねuserのid
+  new: number;
+}
+
+export interface PROPS_COMMENT {
+  text: string;
+  // 対象の投稿
+  post: number;
+}
+
+/**
+ * Post.tsx
+ */
+export interface PROPS_POST {
+  // 対象の投稿のid
+  postId: number;
+  // login中のuser id
+  loginId: number;
+  // 投稿したuserのid
+  userPost: number;
+  title: string;
+  imageUrl: string;
+  // 投稿にいいねをしたuserのid
+  liked: number[];
+}
