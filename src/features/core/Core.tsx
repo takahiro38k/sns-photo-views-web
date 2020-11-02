687c58db49c6c97f423ba367cc68021555305829
takahiro38k <grandeur38k@gmail.com>
2020-11-02 12:19:28 +0900

    2020-1102-1218-30 made files

---
+++ src/features/core/Core.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Core: React.FC = () => {
+  return (
+    <div>
+      test
+    </div>
+  );
+};
+
+export default Core;
---
+++ src/features/post/Post.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Post;
